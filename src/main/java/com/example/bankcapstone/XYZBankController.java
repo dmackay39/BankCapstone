c675080e621552933976fabcaec99a9e4f413b14
Sarah-Irvine <123417453+Sarah-Irvine@users.noreply.github.com>
2023-10-02 13:54:03 +0100

    refactoring names and adding landing page

--- src/main/java/com/example/bankcapstone/XYZBankController.java
+++ src/main/java/com/example/bankcapstone/XYZBankController.java
@@ -3,7 +3,7 @@ package com.example.bankcapstone;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class XYZBankController {
     @FXML
     private Label welcomeText;
 
