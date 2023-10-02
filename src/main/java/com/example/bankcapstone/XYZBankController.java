801d5092ed56ce0be973f1d5da01d301c2fce9a1
Sarah-Irvine <123417453+Sarah-Irvine@users.noreply.github.com>
2023-10-02 15:41:35 +0100

    landing page changes

--- src/main/java/com/example/bankcapstone/XYZBankController.java
+++ src/main/java/com/example/bankcapstone/XYZBankController.java
@@ -1,10 +1,15 @@
 package com.example.bankcapstone;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class XYZBankController {
     @FXML
@@ -24,7 +29,13 @@ public class XYZBankController {
     protected void onHelloButtonClick() {
         try {
             if (landingPageGroup.getSelectedToggle().equals(customerLogIn)) {
-                welcomeText.setText("customer Log in selected");
+                welcomeText.setText("customer login selected");
+                /*Stage stage2 = new Stage();
+                FXMLLoader fxmlLoader2 = new FXMLLoader(XYZBankApplication.class.getResource("customer-login.fxml"));
+                Scene scene2 = new Scene(fxmlLoader2.load(), 320, 240);
+                stage2.setTitle("Customer Login");
+                stage2.setScene(scene2);
+                stage2.show();*/
             } else if (landingPageGroup.getSelectedToggle().equals(customerRegistration)) {
                 welcomeText.setText("customer registration selected");
             } else if (landingPageGroup.getSelectedToggle().equals(employeeLogIn)) {
@@ -33,6 +44,8 @@ public class XYZBankController {
         }
         catch(RuntimeException re){
             welcomeText.setText("Invalid, please select an option to continue.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
     }
