1f829cf48e4157afbbd1a235b5abd4802b6b0006
Sarah-Irvine <123417453+Sarah-Irvine@users.noreply.github.com>
2023-10-02 14:30:54 +0100

    landing page radio buttons grouped

--- src/main/java/com/example/bankcapstone/XYZBankController.java
+++ src/main/java/com/example/bankcapstone/XYZBankController.java
@@ -1,9 +1,17 @@
 package com.example.bankcapstone;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 public class XYZBankController {
+    public RadioButton customerLogIn;
+    public RadioButton customerRegistration;
+    public RadioButton employeeLogIn;
+
+    public Button landingPageButton;
     @FXML
     private Label welcomeText;
 
