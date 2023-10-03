12954c0d35c940acdaca6b65e3462ffceb8c14d1
Daniel MacKay <84335791+dmackay39@users.noreply.github.com>
2023-10-03 10:00:06 +0100

    Changes to loan and loanfactory

--- src/main/java/com/example/bankcapstone/LoanFactory.java
+++ src/main/java/com/example/bankcapstone/LoanFactory.java
@@ -1,5 +1,7 @@
 package com.example.bankcapstone;
 
+import java.time.LocalDate;
+
 public class LoanFactory {
     public Loan createLoan(LoanTypeEnum type, int term, double amount){
         Loan newLoan = null;
@@ -7,6 +9,15 @@ public class LoanFactory {
             case CAR -> newLoan = new CarLoan(amount, term);
             case HOME -> newLoan = new HomeLoan(amount, term);
             case PERSONAL -> newLoan = new PersonalLoan(amount);
+        }
+        return newLoan;
+    }
+    public Loan createLoan(LoanTypeEnum type, int term, double amount, LocalDate startDate){
+        Loan newLoan = null;
+        switch (type){
+            case CAR -> newLoan = new CarLoan(amount, term, startDate);
+            case HOME -> newLoan = new HomeLoan(amount, term, startDate);
+            case PERSONAL -> newLoan = new PersonalLoan(amount, startDate);
         }
         return newLoan;
     }
