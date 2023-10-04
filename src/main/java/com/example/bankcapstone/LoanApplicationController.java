cc77ff5d6741097ce2d161559185daf12271fdb3
zs110313 <zedsheikh110@gmail.com>
2023-10-04 09:51:20 +0100

    Changes made to loan-application.fxml and controller.java

--- src/main/java/com/example/bankcapstone/LoanApplicationController.java
+++ src/main/java/com/example/bankcapstone/LoanApplicationController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -26,8 +27,23 @@ public class LoanApplicationController implements Initializable {
 
     @FXML
     public void loanApplicationSubmitClicked(ActionEvent actionEvent) throws IOException {
+        String selectedLoanType = loanChoicePicker.getValue().toString();
+
+        String lApoundsString = loanApplicationPounds.getText();
+        String lApenniesString = loanApplicationPennies.getText();
+
+        // Put code here to submit the loan application with the selected loan type and amount in pounds and pennies
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Loan Application");
+        alert.setHeaderText(null);
+        alert.setContentText("Loan application submitted");
+
+        alert.showAndWait();
+
         Stage stage = (Stage) loanApplicationSubmitButton.getScene().getWindow();
         stage.close();
+
         Stage stage2 = new Stage();
         FXMLLoader fxmlLoader2 = new FXMLLoader(XYZBankApplication.class.getResource("customer-account.fxml"));
         Scene scene2 = new Scene(fxmlLoader2.load(), 650, 650);
@@ -38,6 +54,26 @@ public class LoanApplicationController implements Initializable {
 
     @FXML
     public void onLoanTypePicker(ActionEvent actionEvent) {
+        String selectedLoanType = loanChoicePicker.getValue().toString();
+
+        if (selectedLoanType != null) {
+            if (selectedLoanType.equals("Home Loan - 15 years")) {
+                // 15-year home loan
+            } else if (selectedLoanType.equals("Home Loan - 20 years")) {
+                // 20-year home loan
+            } else if (selectedLoanType.equals("Home Loan - 30 years")) {
+                // 30-year home loan
+            } else if (selectedLoanType.equals("Car Loan - 3 years")) {
+                // 3-year car loan
+            } else if (selectedLoanType.equals("Car Loan - 4 years")) {
+                // 4-year car loan
+            } else if (selectedLoanType.equals("Car Loan - 5 years")) {
+                // 5-year car loan
+            } else if (selectedLoanType.equals("Personal Loan")) {
+                // Personal loan
+            }
+        }
+
     }
 
     @Override
@@ -52,5 +88,5 @@ public class LoanApplicationController implements Initializable {
         loanChoicePicker.getItems().add("Personal Loan");
     }
 
-    //ZAIN - pick up from here ...Sarah has added some extra bits - see above
+
 }
