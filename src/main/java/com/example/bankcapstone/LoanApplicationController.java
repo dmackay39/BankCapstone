d7054401610544b4bc102cd2ad405086e19a72dd
Daniel MacKay <84335791+dmackay39@users.noreply.github.com>
2023-10-04 14:38:54 +0100

    Loan status string on loan application

--- src/main/java/com/example/bankcapstone/LoanApplicationController.java
+++ src/main/java/com/example/bankcapstone/LoanApplicationController.java
@@ -42,13 +42,13 @@ public class LoanApplicationController implements Initializable {
             String lApoundsString = loanApplicationPounds.getText().trim();
             String lApenniesString = loanApplicationPennies.getText().trim();
             double moneyToLoan = Integer.parseInt(lApoundsString) + Integer.parseInt(lApenniesString) / 100.0;
-
+            String loanApproval;
             if (term == 0) {
-                Bank.getInstance().approveCustomerLoan(customer.getEmail(), moneyToLoan, loanType, accountChoice);
-                loanLabel.setText("New loan requested. It will appear in your loans if approved");
+                loanApproval = Bank.getInstance().approveCustomerLoan(customer.getEmail(), moneyToLoan, loanType, accountChoice);
+                loanLabel.setText(loanApproval);
             } else {
-                Bank.getInstance().approveCustomerLoan(customer.getEmail(), moneyToLoan, loanType, term, accountChoice);
-                loanLabel.setText("New loan requested. It will appear in your loans if approved");
+                loanApproval = Bank.getInstance().approveCustomerLoan(customer.getEmail(), moneyToLoan, loanType, term, accountChoice);
+                loanLabel.setText(loanApproval);
             }
             // Put code here to submit the loan application with the selected loan type and amount in pounds and pennies
 
